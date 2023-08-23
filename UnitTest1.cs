510356f926d85dc6852f6e5746ede045357f92dd
Travis Schultz <travis.schultz@gmail.com>
2023-08-23 16:18:26 -0500

    Initial commit

---
+++ UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace WebUITestFramework
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
